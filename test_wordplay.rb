df3683423686e466af017d73cdd33a7d23e0d1af
oszegda <oszegda@icloud.com>
2023-01-21 00:29:59 +0100

    Testing Words Separation

--- test_wordplay.rb
+++ test_wordplay.rb
@@ -10,4 +10,9 @@ class TestWordPlay < Minitest::Test
     test_text = %q{Hello. This is a test of sentence separation. This is the end of the test.}
     assert_equal('This is the end of the test', test_text.sentences[2])
   end
+
+  def test_words
+    assert_equal(%w{this is a test}, 'this is a test'.words)
+    assert_equal(%w{these are mostly words}, 'these are, mostly, words'.words)
+  end
 end
