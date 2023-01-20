7c2bddf69a4025609c7a205fd3e3d08751e557cd
oszegda <oszegda@icloud.com>
2023-01-20 23:44:54 +0100

    Splitting Sentences into Words

--- wordplay.rb
+++ wordplay.rb
@@ -9,7 +9,13 @@ class String
   def sentences
     gsub(/\n|\r/, ' ').split(/\.\s*/)
   end
+
+  def words
+    scan(/\w[\w\'\-]*/)
+  end
 end
 
+p "This is a test of words' capabilities".words
+
 p %q{Hello. This is a test of basic sentence splitting.
-It even works over multiple lines.}.sentences
+It even works over multiple lines}.sentences[1].words[3]
