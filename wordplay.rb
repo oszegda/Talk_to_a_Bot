798e026eeade55279ace386642cc4870e89fafe0
oszegda <oszegda@icloud.com>
2023-01-20 23:37:07 +0100

    Splitting Text into Sentences

--- wordplay.rb
+++ wordplay.rb
@@ -3,3 +3,13 @@
 # top-level documentation
 class WordPlay
 end
+
+# top-level documentation
+class String
+  def sentences
+    gsub(/\n|\r/, ' ').split(/\.\s*/)
+  end
+end
+
+p %q{Hello. This is a test of basic sentence splitting.
+It even works over multiple lines.}.sentences
