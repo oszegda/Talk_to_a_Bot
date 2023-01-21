d68aba657d4b7b956e27fecf070c9f0a7a15ad0b
oszegda <oszegda@icloud.com>
2023-01-21 22:34:35 +0100

    Adding a comments to the test_wordplay.rb

--- wordplay.rb
+++ wordplay.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 # top-level documentation
-class WordPlay
-  def self.best_sentence(sentences, desired_words)
-    ranked_sentences = sentences.sort_by do |s|
-      s.words.length - (s.downcase.words - desired_words).length
-    end
-    ranked_sentences.last
+class String
+  def sentences
+    gsub(/\n|\r/, ' ').split(/\.\s*/)
   end
 
+  def words
+    scan(/\w[\w\'\-]*/)
+  end
+end
+
+# top-level documentation
+class WordPlay
   def self.switch_pronouns(text)
     text.gsub(/\b(I am|You are|I|You|Me|Your|My)\b/i) do |pronoun|
       case pronoun.downcase
@@ -29,16 +33,11 @@ class WordPlay
       end
     end.sub(/^me\b/i, 'i')
   end
-end
-
-# top-level documentation
-class String
-  def sentences
-    gsub(/\n|\r/, ' ').split(/\.\s*/)
-  end
-
-  def words
-    scan(/\w[\w\'\-]*/)
+  def self.best_sentence(sentences, desired_words)
+    ranked_sentences = sentences.sort_by do |s|
+      s.words.length - (s.downcase.words - desired_words).length
+    end
+    ranked_sentences.last
   end
 end
 
