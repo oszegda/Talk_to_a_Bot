155ca07d4e614f33fbc0c5bd52ea1eaccfcc9f4d
oszegda <oszegda@icloud.com>
2023-01-21 23:52:06 +0100

    Looking for Matching Phrases

--- bot.rb
+++ bot.rb
@@ -27,6 +27,29 @@ class Bot
   def response_to(input)
     prepared_input = preprocess(input).downcase
     sentence = best_sentence(prepared_input)
+    responses = possible_responses(sentence)
+  end
+
+  def possible_responses
+    responses = []
+
+    # Find all patterns to try to match against
+    @data[:responses].keys.each do |pattern|
+      next unless pattern.is_a?(String)
+
+      # For each pattern, see if the supplied sentence contains
+      # a match. Remove substitution symbols (*) before checking.
+      # Push all responses to the responses array.
+      if sentence.match('\b' + pattern.gsub(/\*/, '') + '\b')
+        responses << @data[:responses][pattern]
+      end
+    end
+
+    # If there were no matches, add the default ones
+    responses << @data[:responses][:default] if responses.empty?
+
+    # Flatten the blocks of responses to a flat array
+    responses.flatten
   end
 
   private
